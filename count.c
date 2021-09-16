da2927ba50791ce87a2014c7f94311de89e25dd2
tildeman <luucongthehien@gmail.com>
2021-09-16 16:26:27 +0700

    fer

---
+++ count.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int count;
+    printf("Đây là a\n");
+    for (count=1;count<=6;count=count++){
+        printf("nice (#%d)",count);
+    }
+    printf("world :)");
+}
